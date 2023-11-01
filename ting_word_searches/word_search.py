e72bb5f7dbbd83a10ef537cead5e076e31748c63
Juliana Marques <marquesdjuliana@gmail.com>
2023-11-01 18:27:14 -0300

    Implement fsearch_by_word/word_search: which searches for a word in files.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -28,5 +28,26 @@ def exists_word(word: str, instance: Queue):
     return result
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word: str, instance: Queue):
+    result = []
+
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        file_name = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+        matching_lines = []
+
+        for line_number, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                matching_lines.append({"linha": line_number, "conteudo": line})
+
+        if matching_lines:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": matching_lines,
+                }
+            )
+
+    return result
