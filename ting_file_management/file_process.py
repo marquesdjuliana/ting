e77bc0a7911fcbf8c8ed3aceaba4a845b91877da
Juliana Marques <marquesdjuliana@gmail.com>
2023-11-01 15:14:58 -0300

    Implements process/file_processde: transforms the result of txt_importer into a dictionary.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,28 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+
+    if file_content is None:
+        return
+
+    file_name = path_file
+    for i in range(len(instance)):
+        existing_file = instance.search(i)
+        if existing_file["nome_do_arquivo"] == file_name:
+            return
+
+    processed_data = {
+        "nome_do_arquivo": file_name,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+
+    instance.enqueue(processed_data)
+
+    sys.stdout.write(str(processed_data) + "\n")
 
 
 def remove(instance):
