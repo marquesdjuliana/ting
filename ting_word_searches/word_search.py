2abaa1181b0ed0c4b281b94314bdc5979752028c
Juliana Marques <marquesdjuliana@gmail.com>
2023-11-01 17:39:50 -0300

    Implements exists_word/ word search: check the existence of a word in files

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,31 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    result = []
+    index = 0
+
+    while index < len(instance):
+        matching_lines = []
+        file_data = instance.search(index)
+        file_name = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+
+        for line_number, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                matching_lines.append({"linha": line_number})
+
+        if matching_lines:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": matching_lines,
+                }
+            )
+        index += 1
+
+    return result
 
 
 def search_by_word(word, instance):
